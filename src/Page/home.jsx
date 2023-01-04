05b22df7755b02422e65d1ab335c637641826a00
rickys20 <rickysupriyanto807@gmail.com>
2023-01-04 12:51:06 +0700

    feat: first commit

---
+++ src/Page/home.jsx
@@ -0,0 +1,20 @@
+function App() {
+
+    return (
+        <>
+            <section className="hal1" id={"hal1"}>
+                <div className="container-fluid">
+                    <div className="home-section">
+                            <div className="justify-center">
+                                <h2 className="text-judul">Sewa & Rental Mobil Terbaik di kawasan Surabaya</h2>
+                                <h6 className="text-judul-desc">Selamat datang di Binar Car Rental. Kami menyediakan mobil kualitas terbaik dengan harga terjangkau. Selalu siap melayani kebutuhanmu untuk sewa mobil selama 24 jam.</h6>
+                                <a href="/posts"><button type="button" className="btn button-green">Mulai Sewa Mobil</button></a>
+                            </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+}
+
+export default App;
